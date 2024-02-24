47635a5d00a4b54e0b6409ba4f2584190ad86ba5
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-02-24 13:37:41 +0530

    taskValidationSchema Created

--- server.js
+++ server.js
@@ -20,7 +20,62 @@ const taskSchema = new Schema({
 
 const Task = model('Task', taskSchema)
 
+const taskSchemaValidation = {
+    title: {
+        in: ['body'],
+        notEmpty: {
+            errorMessage: 'Title cannot be empty'
+        },
+        exists: {
+            errorMessage: 'Title is required'
+        },
+        isLength: {
+            options: { min: 5 },
+            errorMessage: 'title should be at least 5 character'
+        },
+        custom: {
+            options: function (value) {
+                Task.findOne({ title: value })
+                    .then((task) => {
+                        if (task) {
+                            throw new Error('title already exists')
+                        } else {
+                            return true
+                        }
+                    })
+                    .catch()
+            }
+        }
+    },
 
+    description: {
+        in: ['body'],
+        notEmpty: {
+            errorMessage: 'Description cannot be empty'
+        },
+        exists: {
+            errorMessage: 'Description is required'
+        },
+        isLength: {
+            options: { min: 5 },
+            errorMessage: 'Description should be at least 5 character'
+        }
+    },
+
+    status: {
+        in: ['body'],
+        notEmpty: {
+            errorMessage: 'Status cannot be empty'
+        },
+        exists: {
+            errorMessage: 'Status is required'
+        },
+        isIn: {
+            options: [['pending', 'in progress', 'completed']],
+            errorMessage: 'status should be one of (pending, in progress, completed)'
+        }
+    }
+}
 
 
 app.listen(port, () => {
